9020770551d32899e5e799a8a1e2326c0dbcb1e7
Tobe0504 <ezimorahtobenna@gmail.com>
2023-12-16 02:37:32 +0100

    added the home page

---
+++ src/Containers/LandingPage/LandingPage.tsx
@@ -0,0 +1,40 @@
+import { useNavigate } from "react-router-dom";
+import Button from "../../Components/Button/Button";
+import Layout from "../../Components/Layout/Layout";
+import classes from "./LandingPage.module.css";
+
+const LandingPage = () => {
+  // Router
+  const navigate = useNavigate();
+  return (
+    <Layout>
+      <div className={classes.container}>
+        <div className={classes.innerContainer}>
+          <h4>
+            Welcome to the <br />
+            Eclipse
+          </h4>
+          <div className={classes.buttonContainer}>
+            <Button
+              onClick={() => {
+                navigate("/registration");
+              }}
+            >
+              Take your first step
+            </Button>
+            <Button
+              type="secondary"
+              onClick={() => {
+                navigate("/ticket-categories");
+              }}
+            >
+              See our ticket categories
+            </Button>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default LandingPage;
---
+++ src/Utilities/tickets.tsx
@@ -0,0 +1,32 @@
+export const tickets = [
+  {
+    name: "Ebony Entry",
+    description: [
+      "Immerse yourself in the elegance of the night with our Ebony Entry. Indulge in a sophisticated evening filled with captivating conversations, delightful cuisine, and unforgettable moments. This ticket is your key to a memorable experience, where you can dance under the stars and create lasting memories.",
+    ],
+    price: "13,000",
+  },
+  {
+    name: "Bonnie and Clyde",
+    description: [
+      "Embark on a romantic journey with our Bonnie and Clyde couple tickets. Share an enchanting night with your partner, where love is in the air, and every moment is a cherished memory. Enjoy a special evening of fine dining, dancing, and creating a connection that lasts a lifetime.",
+    ],
+    price: "25,000",
+  },
+
+  {
+    name: "The 8 Ball",
+    description: [
+      "Gather your friends and reserve 'The 8 Ball' for an unforgettable night of camaraderie and celebration. This ticket guarantees an exclusive table for eight, ensuring you and your companions have a prime spot to enjoy the festivities. Revel in the joy of togetherness and create a tableau of shared laughter and enjoyment.",
+    ],
+    price: "95,000",
+  },
+
+  {
+    name: "Old money",
+    description: [
+      "Step into the realm of tradition and nostalgia with our 'Old Money' alumni tickets. Join fellow AIESECers in a celebration that pays homage to the roots of AIESEC. Reconnect with old friends, forge new connections, and relive the memories that shaped your AIESEC journey. Your presence adds a touch of history to this grand event.",
+    ],
+    price: "18,000",
+  },
+];
